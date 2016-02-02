c0fdcee462d99fcb827922107d99245720ac42ce
wojtkiewicz <wojtkiewicz.bartosz@gmail.com>
2016-02-02 22:04:28 +0100

    315 setting timeouts for apache client

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/sender/http/ApacheMessageBatchSender.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/sender/http/ApacheMessageBatchSender.java
@@ -1,11 +1,13 @@
 package pl.allegro.tech.hermes.consumers.consumer.sender.http;
 
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import pl.allegro.tech.hermes.api.EndpointAddress;
 import pl.allegro.tech.hermes.consumers.consumer.batch.MessageBatch;
 import pl.allegro.tech.hermes.consumers.consumer.sender.MessageBatchSender;
@@ -20,17 +22,25 @@ import static pl.allegro.tech.hermes.consumers.consumer.sender.http.AvroMediaTyp
 public class ApacheMessageBatchSender implements MessageBatchSender {
     private CloseableHttpClient client = HttpClients.createMinimal();
 
+    private final RequestConfig requestConfig = RequestConfig.custom()
+            .setConnectTimeout(5000)
+            .setConnectionRequestTimeout(5000)
+            .setSocketTimeout(5000)
+            .build();
+
     @Override
     public MessageSendingResult send(MessageBatch batch, EndpointAddress address) {
         try {
             ContentType contentType = getMediaType(batch.getContentType());
             HttpPost httpPost = new HttpPost(address.getEndpoint());
             ByteBufferEntity entity = new ByteBufferEntity(batch.getContent(), contentType);
+            httpPost.setConfig(requestConfig);
             httpPost.setEntity(entity);
             httpPost.addHeader(HTTP.CONN_KEEP_ALIVE, "true");
             httpPost.addHeader(BATCH_ID.getName(), batch.getId());
             httpPost.addHeader(HTTP.CONTENT_TYPE, contentType.getMimeType());
             CloseableHttpResponse response = client.execute(httpPost);
+            EntityUtils.consumeQuietly(response.getEntity());
             return new MessageSendingResult(response.getStatusLine().getStatusCode());
         } catch (IOException e) {
             return new MessageSendingResult(e);
