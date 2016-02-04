7397c984b5ed27171f55d329fc9b27528205d71c
wojtkiewicz <wojtkiewicz.bartosz@gmail.com>
2016-02-04 15:46:03 +0100

    315 refactored buffer pool tests

--- hermes-consumers/src/test/java/pl/allegro/tech/hermes/consumers/consumer/batch/DirectBufferPoolTest.java
+++ hermes-consumers/src/test/java/pl/allegro/tech/hermes/consumers/consumer/batch/DirectBufferPoolTest.java
@@ -1,98 +1,126 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package pl.allegro.tech.hermes.consumers.consumer.batch;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.*;
 
 public class DirectBufferPoolTest {
-    /**
-     * Test the simple non-blocking allocation paths
-     */
+    long totalMemory = 10*1024;
+    int poolableSize = 1024;
+
+    DirectBufferPool pool;
+    ByteBuffer buffer;
+
+    @Before
+    public void setup() {
+        this.pool = new DirectBufferPool(totalMemory, poolableSize, false);
+    }
+
+
+    @After
+    public void cleanup() {
+        if (buffer != null) pool.deallocate(buffer);
+    }
+
     @Test
-    public void testSimple() throws Exception {
-        long totalMemory = 64 * 1024;
-        int size = 1024;
-        DirectBufferPool pool = new DirectBufferPool(totalMemory, size, false);
-        ByteBuffer buffer = pool.allocate(size);
+    public void shouldAllocateGivenAmountOfBytes() throws InterruptedException {
+        // given
+        int size = 512;
+
+        // when
+        buffer = pool.allocate(size);
+
+        // then
         assertEquals("Buffer size should equal requested size.", size, buffer.limit());
         assertEquals("Unallocated memory should have shrunk", totalMemory - size, pool.unallocatedMemory());
         assertEquals("Available memory should have shrunk", totalMemory - size, pool.availableMemory());
-        buffer.putInt(1);
+    }
+
+    @Test
+    public void shouldDeallocateAllMemoryFromBuffer() throws InterruptedException {
+        // given
+        buffer = pool.allocate(512).putInt(1);
         buffer.flip();
+
+        // when
         pool.deallocate(buffer);
+
+        // then
         assertEquals("All memory should be available", totalMemory, pool.availableMemory());
-        assertEquals("But now some is on the free list", totalMemory - size, pool.unallocatedMemory());
-        buffer = pool.allocate(size);
+        assertEquals("Nothing is on a free list", totalMemory, pool.unallocatedMemory());
+    }
+
+    @Test
+    public void shouldRecycleBuffersOfPoolableSize() throws InterruptedException {
+        // given
+        pool.deallocate(pool.allocate(poolableSize));
+
+        // when
+        buffer = pool.allocate(poolableSize);
+
+        // then
         assertEquals("Recycled buffer should be cleared.", 0, buffer.position());
         assertEquals("Recycled buffer should be cleared.", buffer.capacity(), buffer.limit());
-        pool.deallocate(buffer);
-        assertEquals("All memory should be available", totalMemory, pool.availableMemory());
-        assertEquals("Still a single buffer on the free list", totalMemory - size, pool.unallocatedMemory());
-        buffer = pool.allocate(2 * size);
-        pool.deallocate(buffer);
-        assertEquals("All memory should be available", totalMemory, pool.availableMemory());
-        assertEquals("Non-standard size didn't go to the free list.", totalMemory - size, pool.unallocatedMemory());
+        assertEquals("Still a single buffer on the free list", totalMemory - poolableSize, pool.unallocatedMemory());
     }
 
-    /**
-     * Test that we cannot try to allocate more memory then we have in the whole pool
-     */
+    @Test
+    public void shouldDeallocateNonStandardBufferSize() throws InterruptedException {
+        // given
+        buffer = pool.allocate(2 * poolableSize);
+
+        // when
+        pool.deallocate(buffer);
+
+        // then
+        assertEquals("All memory should be available", totalMemory, pool.availableMemory());
+        assertEquals("Non-standard size didn't go to the free list.", totalMemory, pool.unallocatedMemory());
+    }
+
     @Test(expected = IllegalArgumentException.class)
-    public void testCantAllocateMoreMemoryThanWeHave() throws Exception {
+    public void shouldNotAllocateMoreMemoryThanTotalMemoryWeHave() throws InterruptedException {
+        // given
         DirectBufferPool pool = new DirectBufferPool(1024, 512, true);
-        ByteBuffer buffer = pool.allocate(1024);
-        assertEquals(1024, buffer.limit());
-        pool.deallocate(buffer);
-        buffer = pool.allocate(1025);
+
+        // when
+        pool.allocate(1025);
+    }
+
+    @Test(expected = BufferOverflowException.class)
+    public void shouldThrowExceptionOnBufferExhaustion() throws InterruptedException {
+        // given
+        DirectBufferPool pool = new DirectBufferPool(1024, 512, false);
+
+        // when
+        pool.allocate(512);
+        pool.allocate(768);
     }
 
     @Test
-    public void testNonblockingMode() throws Exception {
-        DirectBufferPool pool = new DirectBufferPool(2, 1, false);
-        pool.allocate(1);
-        try {
-            pool.allocate(2);
-            fail("The buffer allocated more than it has!");
-        } catch (BufferOverflowException e) {
-            // this is good
-        }
-    }
-
-    /**
-     * Test that delayed allocation blocks
-     */
-    @Test
-    public void testDelayedAllocation() throws Exception {
-        DirectBufferPool pool = new DirectBufferPool(5 * 1024, 1024, true);
+    public void shouldBlockOnAllocationUntilMemoryIsAvailable() throws Exception {
+        // given
+        int total = 5 * 1024;
+        DirectBufferPool pool = new DirectBufferPool(total, 1024, true);
         ByteBuffer buffer = pool.allocate(1024);
-        CountDownLatch doDealloc = asyncDeallocate(pool, buffer);
-        CountDownLatch allocation = asyncAllocate(pool, 5 * 1024);
+
+        // when
+        CountDownLatch doDeallocation = asyncDeallocate(pool, buffer);
+        CountDownLatch allocation = asyncAllocate(pool, total);
+
+        // then
         assertEquals("Allocation shouldn't have happened yet, waiting on memory.", 1L, allocation.getCount());
-        doDealloc.countDown(); // return the memory
+        doDeallocation.countDown();
         allocation.await();
     }
 
@@ -129,25 +157,26 @@ public class DirectBufferPoolTest {
         return completed;
     }
 
-    /**
-     * This test creates lots of threads that hammer on the pool
-     */
     @Test
-    public void testStressfulSituation() throws Exception {
+    public void shouldSurviveHammeringOfLotsOfThreadsOnBufferPool() throws Exception {
+        // given
         int numThreads = 10;
         final int iterations = 50000;
         final int poolableSize = 1024;
         final int totalMemory = numThreads / 2 * poolableSize;
         final DirectBufferPool pool = new DirectBufferPool(totalMemory, poolableSize, true);
-        List<StressTestThread> threads = new ArrayList<StressTestThread>();
-        for (int i = 0; i < numThreads; i++)
-            threads.add(new StressTestThread(pool, iterations));
-        for (StressTestThread thread : threads)
-            thread.start();
-        for (StressTestThread thread : threads)
-            thread.join();
-        for (StressTestThread thread : threads)
-            assertTrue("Thread should have completed all iterations successfully.", thread.success.get());
+
+        List<StressTestThread> threads = IntStream.range(0, numThreads)
+                .mapToObj(i -> new StressTestThread(pool, iterations))
+                .collect(Collectors.toList());
+
+        // when
+        threads.forEach(StressTestThread::start);
+        threads.forEach(StressTestThread::joinQuietly);
+
+        // then
+        threads.forEach(thread ->
+                assertTrue("Thread should have completed all iterations successfully.", thread.success.get()));
         assertEquals(totalMemory, pool.availableMemory());
     }
 
@@ -165,22 +194,22 @@ public class DirectBufferPoolTest {
         public void run() {
             try {
                 for (int i = 0; i < iterations; i++) {
-                    int size;
-
-                    if (random.nextBoolean())
-                        // allocate poolable size
-                        size = pool.poolableSize();
-                    else
-                        // allocate a random size
-                        size = random.nextInt((int) pool.totalMemory());
+                    int size = random.nextBoolean()? pool.poolableSize() : random.nextInt((int) pool.totalMemory());
                     ByteBuffer buffer = pool.allocate(size);
                     pool.deallocate(buffer);
                 }
                 success.set(true);
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void joinQuietly() {
+            try {
+                join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
-
 }
